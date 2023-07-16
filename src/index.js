812228be2774e38b0b3b748bcc6497acaf5fabc5
Vanessa Bautista <vanessawebdev@gmail.com>
2023-07-16 05:38:17 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import helloWorld from "./hello-world";
+import addImage from "./add-image";
+helloWorld();
+addImage();
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const { dirname } = require("path");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  mode: "none",
+  module: {
+    rules: [{ test: /\.(png|jpg)$/, type: "asset/resource" }],
+  },
+};
