fd305a6dcb79fd264452d103210e0676b2233e02
Vanessa Bautista <vanessawebdev@gmail.com>
2023-07-16 06:19:02 -0700

    added elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import helloWorld from "./hello-world";
-import addImage from "./add-image";
-helloWorld();
-addImage();
+import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+
+const helloWorldButton = new HelloWorldButton();
+
+helloWorldButton.render();
--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,29 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath:
+      "https://www.melissas.com/cdn/shop/products/2-pounds-image-of-organic-kiwi-fruit-30307832725548_600x600.jpg?v=1652475192",
   },
   mode: "none",
   module: {
-    rules: [{ test: /\.(png|jpg)$/, type: "asset/resource" }],
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3 kilobytes
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
