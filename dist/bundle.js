9e5b00750ae6918eca0235bb3e116345d03d9a15
Vanessa Bautista <vanessawebdev@gmail.com>
2023-07-21 19:17:11 -0700

    added bundler

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,26 +10,38 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _hello_world_button_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
-
-class HelloWorldButton {
-  render() {
-    const button = document.createElement("button");
-    button.innerHTML = "Hello World";
-    button.classList.add("hello-world-button");
-    button.onclick = function () {
-      const p = document.createElement("p");
-      p.innerHTML = "Hello World";
-      p.classList.add("hello-world-text");
-      body.appendChild(p);
-    };
-    const body = document.querySelector("body");
-    body.appendChild(button);
+var HelloWorldButton = /*#__PURE__*/function () {
+  function HelloWorldButton() {
+    _classCallCheck(this, HelloWorldButton);
+    _defineProperty(this, "buttonCssClass", "hello-world-button");
   }
-}
-
+  _createClass(HelloWorldButton, [{
+    key: "render",
+    value: function render() {
+      var button = document.createElement("button");
+      button.innerHTML = "Hello World";
+      button.classList.add(this.buttonCssClass);
+      button.onclick = function () {
+        var p = document.createElement("p");
+        p.innerHTML = "Hello World";
+        p.classList.add("hello-world-text");
+        body.appendChild(p);
+      };
+      var body = document.querySelector("body");
+      body.appendChild(button);
+    }
+  }]);
+  return HelloWorldButton;
+}();
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HelloWorldButton);
-
 
 /***/ }),
 /* 2 */
@@ -537,11 +549,8 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_hello_world_button_hello_world_button__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
-
-const helloWorldButton = new _components_hello_world_button_hello_world_button__WEBPACK_IMPORTED_MODULE_0__["default"]();
-
+var helloWorldButton = new _components_hello_world_button_hello_world_button__WEBPACK_IMPORTED_MODULE_0__["default"]();
 helloWorldButton.render();
-
 })();
 
 /******/ })()
